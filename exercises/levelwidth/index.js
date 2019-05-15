fa53047892ef82615494b7bf66d5a42a8e670cbe
Piotr Myszkowski <myszon01@gmail.com>
2019-05-15 00:11:01 -0700

    bst df trees

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 's']
+    const arrToReturn = [0]
+    while (arr.length > 1) {
+        const node = arr.shift()
+        if (node === 's') {
+            arrToReturn.push(0)
+            arr.push('s')
+        } else {
+            arr.push(...node.children)
+            arrToReturn[arrToReturn.length - 1]++
+        }
+
+    }
+
+    return arrToReturn
+}
 
 module.exports = levelWidth;
--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,43 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data
+        this.children = []
+    }
 
-class Tree {}
+    add(data) {
+        this.children.push(new Node(data))
+    }
+
+    remove(data) {
+        this.children = this.children.filter(child => { child.data !== data })
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root]
+        while (arr.length) {
+            const node = arr.shift()
+            fn(node)
+            arr.push(...node.children)
+        }
+    }
+
+    traverseDF(fn) {
+        const arr = [this.root]
+        while (arr.length) {
+            const node = arr.shift()
+            fn(node)
+            arr.unshift(...node.children)
+        }
+    }
+}
 
 module.exports = { Tree, Node };
